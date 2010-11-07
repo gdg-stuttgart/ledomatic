fb131b4fdfaefec6537de380e4b2d6940796628b
Markus Knittig <markus@myd0.de>
2010-11-07 15:15:34 +0100

    Initial Guice + Restlet setup

---
+++ ledomatic-server/src/com/ledomatic/server/DevicesResource.java
@@ -0,0 +1,21 @@
+package com.ledomatic.server;
+
+import org.restlet.Context;
+import org.restlet.data.MediaType;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+import com.google.inject.Inject;
+
+public class DevicesResource extends ServerResource {
+
+    @Get("xml")
+    public Representation represent() {
+        Context context = new Context();
+        //context.set("projects", service.list());
+        //String xml = converter.convert("templates/projects.vm", context);
+        return new StringRepresentation("", MediaType.TEXT_XML);
+    }
+}
