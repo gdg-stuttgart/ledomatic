40d68ca2095192392d0bc9fbc86671629e4d1f3f
Markus Knittig <markus@myd0.de>
2010-11-07 16:39:41 +0100

    Abstracted REST params

--- ledomatic-server/src/com/ledomatic/server/DevicesResource.java
+++ ledomatic-server/src/com/ledomatic/server/DevicesResource.java
@@ -1,26 +1,70 @@
 package com.ledomatic.server;
 
+import org.restlet.resource.Delete;
 import org.restlet.resource.Get;
+import org.restlet.resource.Put;
 import org.restlet.resource.ServerResource;
 
 public class DevicesResource extends ServerResource {
 
-//    @Get("xml")
-//    public Representation represent() {
-//        Context context = new Context();
-//        context.set("devices", service.list());
-//        //String xml = converter.convert("templates/projects.vm", context);
-//        return new StringRepresentation("", MediaType.TEXT_XML);
-//    }
+	private RestRequest restRequest;
+
+    @Put
+    public String login() {
+    	restRequest = parseParams();
+    	return restRequest.getDeviceid() + " " + restRequest.getInputtype() + " " + restRequest.getId();
+    }
+
+    @Delete
+    public String logout() {
+    	restRequest = parseParams();
+    	return "";
+    }
 
     @Get
-    public String represent(){
-    	String deviceid = (String) getRequest().getAttributes().get("deviceid");
-    	String inputtype = (String) getRequest().getAttributes().get("inputtype");
-    	String id = "";
-    	if (getRequest().getAttributes().containsKey("id")) {
-    		id = (String) getRequest().getAttributes().get("id");
-    	}
-    	return deviceid + " " + inputtype + " " + id;
+    public String status() {
+    	restRequest = parseParams();
+    	return "";
     }
+
+    private RestRequest parseParams() {
+    	RestRequest restRequest = new RestRequest();
+    	restRequest.setDeviceid((String) getRequest().getAttributes().get("deviceid"));
+    	restRequest.setInputtype((String) getRequest().getAttributes().get("inputtype"));
+    	if (getRequest().getAttributes().containsKey("id")) {
+    		restRequest.setId((String) getRequest().getAttributes().get("id"));
+    	}
+    	return restRequest;
+    }
+
+	private class RestRequest {
+		private String deviceid;
+		private String inputtype;
+		private String id;
+
+		public String getDeviceid() {
+			return deviceid;
+		}
+
+		public void setDeviceid(String deviceid) {
+			this.deviceid = deviceid;
+		}
+
+		public String getInputtype() {
+			return inputtype;
+		}
+
+		public void setInputtype(String inputtype) {
+			this.inputtype = inputtype;
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+	}
+
 }
