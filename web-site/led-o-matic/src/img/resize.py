05f5fdbefada96bf2f2cf8e86d4114eae47988c6
Michal Harakal <michal.harakal@googlemail.com>
2010-11-30 11:39:36 +0100

    thumbnails image as sample

--- web-site/led-o-matic/src/img/resize.py
+++ web-site/led-o-matic/src/img/resize.py
@@ -7,4 +7,4 @@ for infile in glob.glob("*.jpg"):
     file, ext = os.path.splitext(infile)
     im = Image.open(infile)
     im.thumbnail(size, Image.ANTIALIAS)
-    im.save(file + ".thumbnail.jpg", "JPEG")
+    im.save(file + "_tn.jpg", "JPEG")
