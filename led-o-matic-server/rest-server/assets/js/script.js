415e55a40120b2527274f8a45dc5f9476e2b50cb
Michal Harakal <michal.harakal@googlemail.com>
2011-01-03 17:03:45 +0100

    basic html5 interface for setting colors

--- led-o-matic-server/rest-server/assets/js/script.js
+++ led-o-matic-server/rest-server/assets/js/script.js
@@ -1,8 +1,51 @@
-/* Author: 
+/* Author: michal harakal
 
 */
 
 
+function setColor(color_txt) {
+  //           color_txt = global_r.toString(16) + global_g.toString(16) + global_b.toString(16);
+ $.ajax({
+   url: 'rr/L1/RGB/0',
+   type: 'POST',
+   data:"color=" + color_txt,   
+   success: function(data) {
+     alert(data);
+   }
+ });
+}
+
+function makeButtonsTouchable() {
+  
+  $('.basic-button').bind('touchstart', function(e) {
+      e.preventDefault();
+      //$(this).addClass('hover_effect');
+    });
+
+  $('.basic-button').bind('touchend', function(e) {
+      e.preventDefault();
+      setColor($(this).getAttribute('rel')) 
+      //$(this).addClass('hover_effect');
+      // get color of div
+    });
+}
+
+function makeButtonsClickable() {
+  
+  $('.basic-button').bind('click', function(e) {
+    setColor($(this)[0].getAttribute('rel')) 
+    });
+ 
+}
+
+function handlOnDocumentReady()
+{
+  makeButtonsClickable();
+  makeButtonsTouchable();
+  
+}
+
+$(document).ready(handlOnDocumentReady());
 
 
 
